fa982c006cb63c669ecf7bb20c26d31934240cb8
Nasir Abdulrasheed <mypythtesting@gmail.com>
2022-11-20 18:19:44 +0100

    added cors and loading env variables from .env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 """
 init file
 """
-from os import environ
+from os import path
 
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
+from dotenv import load_dotenv
 
+curr_dir = path.abspath(path.dirname(__name__))
+load_dotenv(path.join(curr_dir, '..', '.env'))
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins=r'*')
+sio = SocketIO()
+cors = CORS()
 
 app.config.update(
-    SECRET_KEY='c6e88c23a381ac802fbcaa09938f56a80042d30850b27b71db00ced777ecd64e',
-    JSONIFY_PRETTYPRINT_REGULAR=True,
     DEBUG=True,
     ENV='development',
+    JSONIFY_PRETTYPRINT_REGULAR=True,
 )
-
-
-if __name__ == '__main__':
-    HOST = environ.get('HOST') or 'localhost'
-    PORT = environ.get('PORT') or 5000
-
-    socketio.run(app, host=HOST, port=PORT)
+cors.init_app(app, resources={r'/auth/*': {'origins': '*'},
+                r'/api/v1/*': {'origins': '*'}})
+sio.init_app(app, async_mode='eventlet', cors_allowed_origins=r'*')
