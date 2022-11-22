26abc2d0721b2b86ca5d2f8cbc7e9ed5248f780c
Jasmin1002 <jasmineolofin@gmail.com>
2022-11-22 09:34:56 +0100

    feat: Add joining table for chat and user tables

--- models/chat.py
+++ models/chat.py
@@ -2,12 +2,15 @@
 """Chat class models chats table"""
 from models.base_model import BaseModel, Base
 
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 
 class Chat(BaseModel, Base):
     __tablename__ = 'chats'
-    member_1 = Column(String(128), ForeignKey('users.id'), nullable=False) 
-    member_2 = Column(String(128), ForeignKey('users.id'), nullable=False)
-    messages = Column(String(128), nullable=False)
+    sender = Column(String(128), ForeignKey('users.id'), nullable=False) 
+    recipient = Column(String(128), ForeignKey('users.id'), nullable=False)
+    messages = Column(Text, nullable=False)
+
+    # Relationship
+    user = relationship('User', back_populates='chats')
