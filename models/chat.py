cc15fe94328846a4db45ebaf1a16652ba22e9eb0
Jasmin1002 <jasmineolofin@gmail.com>
2022-11-23 13:04:38 +0100

    refactor: Rename conversation to joining_table
    And I add more docstring to the chat class

--- models/chat.py
+++ models/chat.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Chat class models chats table"""
-from models.base_model import BaseModel, Base
-from models.conversation import conversation
 
+from models.base_model import BaseModel, Base
+from models.joining_tables import conversation
 from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 
 class Chat(BaseModel, Base):
+    """
+    Chat extends BaseModel and maps chats table
+    Args:
+        BaseModel: superclass
+        Base: stores a class and its corresponding mapped table
+    """
     __tablename__ = 'chats'
     user_1 = Column(String(128), ForeignKey('users.id'), nullable=False) 
     user_2 = Column(String(128), ForeignKey('users.id'), nullable=False)
