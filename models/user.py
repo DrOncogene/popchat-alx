b4fc2db6743eb40b96d36d25887d166ee55b1515
Jasmin1002 <jasmineolofin@gmail.com>
2022-11-23 10:45:54 +0100

    feat: update users table with joining table
    The joining name conversation stores chat_user model.
    And the relationship between user and chat entities is
    described/labelled with chats property in user entity

--- models/user.py
+++ models/user.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Chat class models chats table"""
+"""User class models chats table"""
 from models.base_model import BaseModel, Base
+from models.conversation import conversation
 
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class User(BaseModel, Base):
     __tablename__ = 'users'
-    username = Column(String(128), nullable=False) 
-    email = Column(String(128), nullable=False)
+    username = Column(String(60), nullable=False, unique=True) 
+    email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
-    chats = Column(String(128), nullable=True)
-    rooms = Column(String(128), nullable=True)
+
+    # Relationship
+    chats = relationship('Chat', secondary=conversation, back_populates='users')
